d4eb225a0d7edf3ad25c59970fa55499b4bbdc29
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2022-06-22 13:11:25 -0500

    GraphFromInput almost finished

--- src/Util/graphFromInput.ts
+++ src/Util/graphFromInput.ts
@@ -1,48 +1,93 @@
 import { GraphNode } from "../Classes/GraphNode";
 
 export function graphFromInput(input: [size: number, gridData: number[][]]) {
-  const [size, gridData] = input
+  const [size, gridData] = input;
 
   if (gridData.length !== size) {
-    throw new Error("Grid data is not the same size as the grid")
+    throw new Error("Grid data is not the same size as the grid");
   }
-  for (let i = 0; i < gridData.length; i++){
+  for (let i = 0; i < gridData.length; i++) {
     if (gridData[i].length !== size) {
-      throw new Error("Grid data is not the same size as the grid")
+      throw new Error("Grid data is not the same size as the grid");
     }
   }
 
-  const nodes = new Map<string, GraphNode>()
+  const nodes = new Map<string, GraphNode>();
+  let startNodeId: string | undefined;
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
-      const node = new GraphNode(`${i}-${j}`)
+      const node = new GraphNode(`${i}-${j}`);
       switch (gridData[i][j]) {
         case 1:
-          node.setType("wall")
+          node.setType("wall");
           break;
         case 2:
-          node.setType("2kilos")
+          node.setType("2kilos");
           break;
         case 3:
-          node.setType("3kilos")
+          node.setType("3kilos");
           break;
         case 4:
-          node.setType("start")
+          node.setType("start");
+          startNodeId = node.getId();
           break;
         case 5:
-          node.setType("recicle")
+          node.setType("recicle");
         default:
-          node.setType("free")
+          node.setType("free");
           break;
       }
-      nodes.set(node.getId(), node)
+      nodes.set(node.getId(), node);
     }
   }
 
-  console.log(nodes)
+  // add the nodes to the childs if they are not walls and are adjacent to the node
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      const node = nodes.get(`${i}-${j}`);
+      if (node === undefined) continue;
+      if (node.getType() !== "wall") {
+        if (i > 0) {
+          const child = nodes.get(`${i - 1}-${j}`);
+          if (child !== undefined) {
+            if (child.getType() !== "wall") {
+              node.addChild(child);
+            }
+          }
+        }
+        if (i < size - 1) {
+          const child = nodes.get(`${i + 1}-${j}`);
+          if (child !== undefined) {
+            if (child.getType() !== "wall") {
+              node.addChild(child);
+            }
+          }
+        }
+        if (j > 0) {
+          const child = nodes.get(`${i}-${j - 1}`);
+          if (child !== undefined) {
+            if (child.getType() !== "wall") {
+              node.addChild(child);
+            }
+          }
+        }
+        if (j < size - 1) {
+          const child = nodes.get(`${i}-${j + 1}`);
+          if (child !== undefined) {
+            if (child.getType() !== "wall") {
+              node.addChild(child);
+            }
+          }
+        }
+      
+      }
+    }
+  }
 
-
-
-
+  if (startNodeId === undefined) {
+    throw new Error("No start node found");
+  }
+  console.log(nodes.get(startNodeId));
+  return nodes.get(startNodeId!);
 
 }
